67ee818616393f0c070f49b311d6dfebfdd5a592
tyler <tylerscott.home@gmail.com>
2023-12-06 11:49:49 -0500

    Live Search Updated

--- src/App.js
+++ src/App.js
@@ -51,68 +51,8 @@ function App() {
                     {currentPage === 'ingredientPage' && <IngredientPage/>}
                 </div>
             </div>
-
-            {/*/!* Main Content Layout *!/*/}
-            {/*<div className="main-layout" style={{display: 'flex', flexGrow: 1, marginTop: '3%'}}>*/}
-            {/*    /!* Left Column - Search Filters *!/*/}
-            {/*    <div className="left-column" style={{flex: '0 0 200px', position: 'fixed', padding: '10px', backgroundColor: '#e0e0e0'}}>*/}
-            {/*        /!* Include your search filters component here *!/*/}
-            {/*        <h3>Search Filters</h3>*/}
-            {/*        <ul>*/}
-            {/*            <li>Filter 1</li>*/}
-            {/*            <li>Filter 2</li>*/}
-            {/*            /!* Add more filters as needed *!/*/}
-            {/*        </ul>*/}
-            {/*    </div>*/}
-
-            {/*    /!* Main Content based on currentPage *!/*/}
-            {/*    <div className="main-content" style={{flex: '1'}}>*/}
-            {/*        {currentPage === 'defaultPage' && <DefaultPage/>}*/}
-            {/*        {currentPage === 'compareIngredients' && <CompareIngredients/>}*/}
-            {/*        {currentPage === 'searchByName' && <SearchByName/>}*/}
-            {/*        {currentPage === 'searchByCategory' && <SearchByCategory/>}*/}
-            {/*        {currentPage === 'ingredientPage' && <IngredientPage/>}*/}
-            {/*    </div>*/}
-            {/*</div>*/}
         </div>
     );
 }
-
-
-// function App() {
-//     const [currentPage, setCurrentPage] = useState('defaultPage');
-//
-//     const handlePageChange = (page) => {
-//         setCurrentPage(page);
-//     };
-//
-//     return (
-//         <div className="App">
-//             <button onClick={() => handlePageChange('defaultPage')}>
-//                 Default Page
-//             </button>
-//             <button onClick={() => handlePageChange('compareIngredients')}>
-//                 Compare Ingredients
-//             </button>
-//             <button onClick={() => handlePageChange('searchByName')}>
-//                 Search By Name
-//             </button>
-//             <button onClick={() => handlePageChange('searchByCategory')}>
-//                 Search By Category
-//             </button>
-//             <button onClick={() => handlePageChange('ingredientPage')}>
-//                 Ingredient Page
-//             </button>
-//             {/*<MouseTracker />*/}
-//
-//
-//             {currentPage === 'defaultPage' && <DefaultPage />}
-//             {currentPage === 'compareIngredients' && <CompareIngredients />}
-//             {currentPage === 'searchByName' && <SearchByName />}
-//             {currentPage === 'searchByCategory' && <SearchByCategory />}
-//             {currentPage === 'ingredientPage' && <IngredientPage />}
-//         </div>
-//     );
-// }
 
 export default App;
--- src/pages/defaultPage.js
+++ src/pages/defaultPage.js
@@ -2,9 +2,11 @@ import React, {useEffect, useState} from "react";
 import IngredientThumbnail from "../components/cards/ingredientThumbnail";
 import IngredientPage from "./ingredientPage";
 
+
 const DefaultPage = () => {
     const [flavors, setFlavors] = useState([]);
     const [selectedIngredient, setSelectedIngredient] = useState(null);
+    const [searchQuery, setSearchQuery] = useState('');
 
     useEffect(() => {
         fetch('http://localhost:5000/api/flavordb')
@@ -16,35 +18,52 @@ const DefaultPage = () => {
                 setFlavors(sortedFlavors);
             })
             .catch(error => console.error('Error fetching data:', error));
-    }, []); // Empty dependency array means this effect runs once after the initial render
+    }, []);
 
-    const handleThumbnailClick = (ingredient) => {
+    const handleThumbnailClick = ingredient => {
         setSelectedIngredient(ingredient);
     };
 
     const handleGoBack = () => {
-        // Reset the selectedIngredient state to go back to the list of ingredients
         setSelectedIngredient(null);
     };
+
+    const handleSearchInputChange = event => {
+        setSearchQuery(event.target.value);
+    };
+
+    // Filter flavors based on the search query
+    const filteredFlavors = flavors.filter(flavor =>
+        flavor.alias.toLowerCase().includes(searchQuery.toLowerCase())
+    );
 
     return (
         <div>
             <h2>Ingredient List</h2>
+            <input
+                type="text"
+                placeholder="Search for ingredients..."
+                value={searchQuery}
+                onChange={handleSearchInputChange}
+            />
             {selectedIngredient ? (
-                <IngredientPage ingredient={selectedIngredient}/>
+                <IngredientPage ingredient={selectedIngredient} />
             ) : (
-                // <div style={{backgroundColor: "#ff9cf0", width: "90%", display: 'flex', flexWrap: 'wrap', margin: "5%"}}>
-                <div style={{backgroundColor: "#ff9cf0", width: "100%", display: 'flex', flexWrap: 'wrap' }}>
-                    {flavors.map(flavor => (
-                        <div key={flavor.index} style={{flex: '0 0 auto', margin: '5px'}}
-                             onClick={() => handleThumbnailClick(flavor)}>
-                            <IngredientThumbnail ingredient={flavor}/>
+                <div style={{ backgroundColor: '#ff9cf0', width: '100%', display: 'flex', flexWrap: 'wrap' }}>
+                    {filteredFlavors.map(flavor => (
+                        <div
+                            key={flavor.index}
+                            style={{ flex: '0 0 auto', margin: '5px' }}
+                            onClick={() => handleThumbnailClick(flavor)}
+                        >
+                            <IngredientThumbnail ingredient={flavor} />
                         </div>
                     ))}
                 </div>
             )}
-            {/* TODO: This needs to be ONLY on ingredient page, not on thumbnails */}
-            <button onClick={handleGoBack}>Go Back to Ingredient List</button>
+            {selectedIngredient && (
+                <button onClick={handleGoBack}>Go Back to Ingredient List</button>
+            )}
         </div>
     );
 };
